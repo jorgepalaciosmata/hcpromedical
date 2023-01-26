e3ef43aab9e9c56762debc8c36133de27f8f4fa7
Jorge Palacios <jorgepalaciosmata@gmail.com>
2023-01-25 20:36:24 -0500

    Auth-InitialCommit

--- Client/App.js
+++ Client/App.js
@@ -2,21 +2,27 @@ import * as React from 'react';
 import { Button, View, Text, Image, TouchableOpacity } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-const axios = require('axios').default;
-import * as Application from 'expo-application';
 
 //Views Imports
-import GetPersonalInfoScreen from './screens/GetPersonalInfoScreen'
-import PostPersonalInfoScreen from './screens/PostPersonalInfoScreen'
 import PersonalInfoScreen from './screens/PersonalInfoScreen';
+import HistorialMedicoScreen from './screens/HistorialMedicoScreen';
+import LaboratorioScreen from './screens/LaboratorioScreen';
+import RecetasScreen from './screens/RecetasScreen';
+import AuthenticationScreen from './screens/AuthenticationScreen';
 
 function HomeScreen({ navigation }) {
   return (
-      
-    // <div style={{width: '380px', border: '1px solid blue'}}>
-      <View style={{ alignItems: 'left', flexDirection: "row", padding: '20px', border: '1px solid black' }}>
+      <View style={{ flexWrap:'wrap', alignItems: 'left', flexDirection: "row", padding: '20px' }}>
         <TouchableOpacity 
-          style={{width: "150", alignItems: 'center'}}
+          style={{width: 150, alignItems: 'center'}}
+          onPress={() => navigation.navigate('PersonalInfo')} >
+          <Image source={require('./assets/icons/nutricion.png')} 
+            style = {{ width: 100, height: 100 }} />
+          <Text>Informacion Personal</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity 
+          style={{width: 150, alignItems: 'center'}}
           onPress={() => navigation.navigate('Historial')} >
           <Image source={require('./assets/icons/medical-history.png')} 
             style = {{ width: 100, height: 100 }} />
@@ -24,82 +30,29 @@ function HomeScreen({ navigation }) {
         </TouchableOpacity>
 
         <TouchableOpacity 
-          style={{width: "150", alignItems: 'center'}}
-          onPress={() => navigation.navigate('PostPersonalInfo')} >
-          <Image source={require('./assets/icons/medical-history.png')} 
+          style={{width: 150, alignItems: 'center'}}
+          onPress={() => navigation.navigate('Laboratorio')} >
+          <Image source={require('./assets/icons/lab.png')} 
             style = {{ width: 100, height: 100 }} />
-          <Text>POST</Text>
+          <Text>Laboratorio</Text>
         </TouchableOpacity>
 
         <TouchableOpacity 
-          style={{width: "150", alignItems: 'center'}}
-          onPress={() => navigation.navigate('GetPersonalInfo')} >
-          <Image source={require('./assets/icons/medical-history.png')} 
+          style={{width: 150, alignItems: 'center'}}
+          onPress={() => navigation.navigate('Recetas')} >
+          <Image source={require('./assets/icons/recetas.png')} 
             style = {{ width: 100, height: 100 }} />
-          <Text>GET</Text>
+          <Text>Recetas</Text>
         </TouchableOpacity>
-        
+
         <TouchableOpacity 
-          style={{width: "150", alignItems: 'center'}}
-          onPress={() => navigation.navigate('PersonalInfo')} >
+          style={{width: 150, alignItems: 'center'}}
+          onPress={() => navigation.navigate('Authentication')} >
           <Image source={require('./assets/icons/medical-history.png')} 
             style = {{ width: 100, height: 100 }} />
-          <Text>Informacion Personal</Text>
+          <Text>Authentication</Text>
         </TouchableOpacity>
       </View>
-    // </div>
-  );
-}
-
-function HistorialMedicoScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Historial médico</Text>
-      <Button onPress={async () => {await readPersonalInfo(); }} title="test" />
-      <Text> test: { Application.applicationId } </Text>
-    </View>
-  );
-}
-
-
-
-async function readPersonalInfo()
-{
-
-  await axios.post(
-    'https://p8ada5o8e0.execute-api.us-east-1.amazonaws.com/Prod/personalinfo', {
-      id: '11'
-    }
-  )
-  // await axios.get(
-  //    'https://p8ada5o8e0.execute-api.us-east-1.amazonaws.com/Prod/personalInfo/1'    
-  // )
-  .then(function (response) {
-    // handle success
-    console.log(response);
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  })
-  .then(function () {
-    // always executed
-  });
-}
-
-function CalendarioScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Calendario</Text>
-    </View>
-  );
-}
-
-function LaboratorioScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Laboratorio</Text>
-    </View>
   );
 }
 
@@ -108,15 +61,13 @@ const Stack = createNativeStackNavigator();
 function App() {
   return (
     <NavigationContainer>
-      {/* I saw that often we create a funtcion named MyStack and we return that in this part */}
       <Stack.Navigator initialRouteName="HC Cloud">
         <Stack.Screen name="HC Cloud" component={HomeScreen} />
-        <Stack.Screen name="Historial" component={HistorialMedicoScreen} />
-        <Stack.Screen name="Calendario" component={CalendarioScreen} />
-        <Stack.Screen name="Laboratorio" component={LaboratorioScreen} />
-        <Stack.Screen name="GetPersonalInfo" component={GetPersonalInfoScreen} />
-        <Stack.Screen name="PostPersonalInfo" component={PostPersonalInfoScreen} />
         <Stack.Screen name="PersonalInfo" component={PersonalInfoScreen} />
+        <Stack.Screen name="Historial" component={HistorialMedicoScreen} />
+        <Stack.Screen name="Laboratorio" component={LaboratorioScreen} />
+        <Stack.Screen name="Recetas" component={RecetasScreen} />
+        <Stack.Screen name="Authentication" component={AuthenticationScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
