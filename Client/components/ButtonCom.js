e40f197c44205f0dfa3c0ccd01dc4106dfd15820
HiramZednem <hirammendez000@gmail.com>
2023-02-08 20:26:17 -0600

    buttoncom added

---
+++ Client/components/ButtonCom.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { TouchableOpacity, View, Text, StyleSheet } from 'react-native'
+
+export const ButtonCom = ({text}) => {
+  return (
+    <TouchableOpacity>
+        <View style={styles.container}>        
+            <Text style={styles.text}>{text}</Text>
+        </View>
+    </TouchableOpacity>
+  )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        height: 50,
+        width: 310,
+        borderRadius: 100,
+        backgroundColor: "#2F4550",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    text: {
+        color: "#ffffff",
+        fontWeight: "bold",
+        fontSize: 20,
+    }
+})
